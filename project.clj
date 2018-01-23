a7e7980b5931ee359777d64b06c17f7e0d47f2e0
Lyosha Kuleshov <aleksey.kuleshov@flocktory.com>
2018-01-23 17:36:30 +0300

    [release] 0.1.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject superkonduktr/wet "0.1.9-SNAPSHOT"
+(defproject superkonduktr/wet "0.1.9"
   :description "Liquid in Clojure"
   :url "https://github.com/superkonduktr/wet"
   :license {:name "Eclipse Public License"
