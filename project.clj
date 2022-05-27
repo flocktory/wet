1447abfb1fe431f576145f07fa2e92bb003dd71e
Colin Lappala <colin@amperity.com>
2022-05-27 15:28:49 -0700

    increment dependency versions reported stale

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/amperity/wet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [instaparse "1.4.9"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [instaparse "1.4.12"]]
   :deploy-repositories [["releases" {:url "https://clojars.org/repo"
                                      :sign-releases false}]])
